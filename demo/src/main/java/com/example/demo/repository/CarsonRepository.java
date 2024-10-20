db9118127f64011ac59e5433767ba9dc0a0979e7
kolo <kolo@kakao.com>
2024-10-20 21:20:32 +0900

    feat : carson AI

---
+++ demo/src/main/java/com/example/demo/repository/CarsonRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import com.example.demo.models.Carson;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+
+public interface CarsonRepository extends JpaRepository<Carson, LocalDate> {
+    boolean existsByCarsonDate(LocalDate carsonDate);
+
+}
